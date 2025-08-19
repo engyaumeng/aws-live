73281be64e7900fac6f8a63e1a7980e9a7a01022
engyaumeng <engym-wm23@student.tarc.edu.my>
2025-08-19 15:29:53 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cb7cehlqzd9t.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "bait3273"
+customdb = "employee"
+custombucket = "engyaumeng-employee"
+customregion = "us-east-1"
+
